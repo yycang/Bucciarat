da7df499ed6d4ee97a873be816ca4c99d1b94ed3
yycang <cyy_dfh@163.com>
2019-05-20 22:25:16 +0800

    不修改数组找重复数字

--- 剑指offer/three.py
+++ 剑指offer/three.py
@@ -13,6 +13,20 @@ def duplicate(arr):
     return False
 
 
+# 长度为n+1的数组中所有数字在1～n范围内，必定有重复数字，找出数组中任意的重复数字
+def duplicate2(arr):
+    arr2 = [None] * len(arr)
+    for i in range(len(arr)):
+        if arr2[arr[i]] is None:
+            arr2[arr[i]] = arr[i]
+        else:
+            return arr[i]
+
+
 if __name__ == '__main__':
     lst = [1, 2, 4, 2, 5, 1, 2, 3]
     print(duplicate(lst))
+
+    lst2 = [2, 1, 3, 6, 5, 6, 4]
+
+    print(duplicate2(lst2))
